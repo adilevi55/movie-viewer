2e78a02d62d6c2a84c6c6a290196b17e6c46fc56
Adi lEVI <50671844+adilevi55@users.noreply.github.com>
2022-06-07 16:53:25 +0300

    tumbnail

--- src/app/components/root-components/app/app.component.ts
+++ src/app/components/root-components/app/app.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private meta: Meta) {
+    this.meta.addTags([
+      { name: 'title', content: 'Movies Viewer' },
+      { name: 'type', content: 'website' },
+      { name: 'url', content: 'https://adilevi55.github.io/movie-viewer' },
+      { name: 'image', content: 'https://i.ibb.co/JFbBxTy/0ecd0365-7fa0-4548-94f8-b2acbbe5c877.png' },
+      { name: 'description', content: `description" content="The site was built by Adi Levy.
+       The purpose of the site is to show my programming skills.
+       The site shows information about movies` },
+      
+//       <!-- <meta name="description" content="The site was built by Adi Levy.
+// The purpose of the site is to show my programming skills.
+// The site shows information about movies" />
+
+// <meta property="og:title" content="Movies Viewer" />
+// <meta property="og:url" content="https://adilevi55.github.io/movie-viewer" />
+// <meta property="og:image" content="https://i.ibb.co/JFbBxTy/0ecd0365-7fa0-4548-94f8-b2acbbe5c877.png" />
+// <meta property="og:type" content="website" />
+
+// <meta name="title" content="Movies Viewer" />
+// <meta name="url" content="https://adilevi55.github.io/movie-viewer" />
+// <meta name="image" content="https://i.ibb.co/JFbBxTy/0ecd0365-7fa0-4548-94f8-b2acbbe5c877.png" />
+// <meta name="type" content="website" />
+// -->
+
+
+
+    ]);
+  }
+}
